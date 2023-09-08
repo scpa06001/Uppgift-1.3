79bd9b30d451170a766cfb6b046ee6bb7d109644
Admin <Admin@TEB123001>
2023-09-08 15:09:58 +0200

    Add project files.

---
+++ Uppgift 1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Scott har gjort denna uppgiften");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+
+        }
+    }
+}
